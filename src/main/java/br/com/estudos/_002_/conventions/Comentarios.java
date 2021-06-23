be4779f9e368827547f4e8716e8e5ef591bf6df7
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-06-23 07:52:42 -0300

    Convenções

---
+++ src/main/java/br/com/estudos/_002_/conventions/Comentarios.java
@@ -0,0 +1,27 @@
+package br.com.estudos._002_.conventions;
+
+/**
+ * Comentário inicial de uma classe
+ * @author gusfr
+ *
+ */
+public class Comentarios {
+
+	// Comentário de uma linha
+	
+	/*
+	 * Comentário de mais
+	 * de uma linha
+	 */
+	
+	/***
+	 * Comentário do javadocs - Serve para gerar documentação oficial do java
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/br/com/estudos/_002_/conventions/ConvPacotes.java
@@ -0,0 +1,13 @@
+package br.com.estudos._002_.conventions;
+
+//o nome do pacote será dominio inverso
+// br.com.nomePrincipaldoDominio.nomepastascomcodigo
+//descrição do pacote na primeira linha do código
+public class ConvPacotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
