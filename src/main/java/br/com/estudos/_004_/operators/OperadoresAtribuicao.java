af3807422e557e7a306aca617341e109c31dce24
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-06-23 14:18:29 -0300

    Create OperadoresAtribuicao.java

---
+++ src/main/java/br/com/estudos/_004_/operators/OperadoresAtribuicao.java
@@ -0,0 +1,19 @@
+package br.com.estudos._004_.operators;
+public class OperadoresAtribuicao {
+
+	/**
+	 *  =  verifica igualdade
+	 *  -=
+	 *  +=
+	 *  *=
+	 *  /=
+	 *  %=
+	 *  
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
