2176c37323e2dfebce8405c30947f77b83f3a81a
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-06-23 14:18:48 -0300

    Create OperadoresComparacao.java

---
+++ src/main/java/br/com/estudos/_004_/operators/OperadoresComparacao.java
@@ -0,0 +1,19 @@
+package br.com.estudos._004_.operators;
+
+public class OperadoresComparacao {
+
+	/**
+	 *  < menor que
+	 *  > maior que
+	 *  <= menor igual
+	 *  >= maior igual
+	 *  == igualdade
+	 *  
+	 *  O instanceof em java é conhecido como um operador de comparação de tipos
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
