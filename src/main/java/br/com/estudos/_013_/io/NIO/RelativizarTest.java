e5fce6b401b0a6b16da61f0baba955bc2bbae45f
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-06-26 16:06:05 -0300

    Create RelativizarTest.java

---
+++ src/main/java/br/com/estudos/_013_/io/NIO/RelativizarTest.java
@@ -0,0 +1,34 @@
+package br.com.estudos._013_.io.NIO;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizarTest {
+
+	public static void main(String[] args) {
+		Path dir = Paths.get("/home/gustavo");
+		Path classe = Paths.get("/home/gustavo/java/Pessoa.java");
+		Path pathToClasse = dir.relativize(classe);
+		System.out.println(pathToClasse);
+		
+		Path absoluto1 = Paths.get("/home/gustavo");
+		Path absoluto2 = Paths.get("/usr/local");
+		Path absoluto3 = Paths.get("/home/gustavo/java/Pessoa.java");
+		
+		Path relativo1 = Paths.get("temp");
+		Path relativo2 = Paths.get("temp/Funcionario.java");
+		
+		System.out.println("1: " + absoluto1.relativize(absoluto3));
+		System.out.println("2: " + absoluto3.relativize(absoluto1));
+		System.out.println("3: " + absoluto1.relativize(absoluto2));
+		System.out.println("4: " + relativo1.relativize(relativo2));
+		
+		//da erro pois o java não sabe localizar o relativo
+		//System.out.println("5: " + absoluto1.relativize(relativo1));
+		
+		
+		
+
+	}
+
+}
