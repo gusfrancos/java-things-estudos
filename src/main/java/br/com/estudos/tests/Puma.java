6f5a8a9f63dafeee221767fec61b2213c3943259
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-06-26 16:05:43 -0300

    Update Puma.java

--- src/main/java/br/com/estudos/tests/Puma.java
+++ src/main/java/br/com/estudos/tests/Puma.java
@@ -6,7 +6,7 @@ package br.com.estudos.tests;
  * o método  getTailLength da interface HashTail é assumido como public já que não é declarado
  * e a classe Puma devia implementar o método com visibilidade de publico 
  */
-abstract class Puma implements HashTail {
-	protected int getTailLength() {return 4;}
-	
-}
+//abstract class Puma implements HashTail {
+//	protected int getTailLength() {return 4;}
+//	
+//}
