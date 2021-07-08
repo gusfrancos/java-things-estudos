e7da515d6dde393f77fbcd5eb091527143e803bb
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-08 15:05:44 -0300

    Update DosFileAttributeTest.java

--- src/main/java/br/com/estudos/_013_/io/NIO/DosFileAttributeTest.java
+++ src/main/java/br/com/estudos/_013_/io/NIO/DosFileAttributeTest.java
@@ -3,16 +3,17 @@ package br.com.estudos._013_.io.NIO;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributeView;
 import java.nio.file.attribute.DosFileAttributes;
 
 public class DosFileAttributeTest {
 
 	public static void main(String[] args) {
-		Path path = Paths.get("folder\\teste.txt");
+		Path path = Paths.get("folder2\\texto.txt");
 		
 		try {
 			if(!Files.exists(path)){
-				Files.createDirectory(path);
+				Files.createFile(path);
 			}
 			
 			Files.setAttribute(path, "dos:hidden", true);
@@ -24,9 +25,28 @@ public class DosFileAttributeTest {
 			System.out.println(dos.isReadOnly());
 			
 			
+			Files.setAttribute(path, "dos:hidden", false);
+			Files.setAttribute(path, "dos:readonly", false);
+			
+			DosFileAttributes dos2 = Files.readAttributes(path, DosFileAttributes.class);
+			
+			System.out.println(dos2.isHidden());
+			System.out.println(dos2.isReadOnly());
+			
+			
+			//jeito mais facil de ver os atributos
+			DosFileAttributeView dosView = Files.getFileAttributeView(path, DosFileAttributeView.class);
+			dosView.setHidden(true);
+			dosView.setReadOnly(true);
+			
+			DosFileAttributes dos3 = Files.readAttributes(path, DosFileAttributes.class);
+			
+			System.out.println(dos3.isHidden());
+			System.out.println(dos3.isReadOnly());
+			
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 		
 
