662a19e91bcb936bf574878259fc96d81196aa40
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-08 15:05:38 -0300

    Create DirectoryStreamTest.java

---
+++ src/main/java/br/com/estudos/_013_/io/NIO/DirectoryStreamTest.java
@@ -0,0 +1,25 @@
+package br.com.estudos._013_.io.NIO;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest {
+
+	public static void main(String[] args) {
+		Path dir = Paths.get("pasta");
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+			for(Path path : stream ) {
+				System.out.println(path.getFileName());
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
