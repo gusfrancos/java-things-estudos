b3c344a943aaf373b6aa1b3bb8fc27363f22df5b
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-08 15:05:50 -0300

    Create FileVisitorTest.java

---
+++ src/main/java/br/com/estudos/_013_/io/NIO/FileVisitorTest.java
@@ -0,0 +1,28 @@
+package br.com.estudos._013_.io.NIO;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileVisitorTest {
+
+	public static void main(String[] args) throws IOException {
+		Files.walkFileTree(Paths.get("pasta"), new AcharTodosOsBkps());
+	}
+
+}
+
+class AcharTodosOsBkps extends SimpleFileVisitor<Path>{
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+		if(file.getFileName().toString().endsWith(".bkp")) {
+			System.out.println(file.getFileName());
+		}
+		return FileVisitResult.CONTINUE;
+		
+	}
+}
