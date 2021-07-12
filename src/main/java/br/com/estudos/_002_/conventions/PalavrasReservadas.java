770a734318f2109492266a9ed6240b639f1938e1
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-12 09:48:48 -0300

    Create PalavrasReservadas.java

---
+++ src/main/java/br/com/estudos/_002_/conventions/PalavrasReservadas.java
@@ -0,0 +1,17 @@
+package br.com.estudos._002_.conventions;
+
+
+
+/**
+ * 
+ * @param args
+ * http://www.linhadecodigo.com.br/artigo/83/as-52-palavras-reservadas-do-java.aspx
+ */
+public class PalavrasReservadas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
