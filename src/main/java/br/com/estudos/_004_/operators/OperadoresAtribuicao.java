3bddf52981f44a2a3a89fbbff8aca3312eb5a72c
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-19 14:05:44 -0300

    operadores

--- src/main/java/br/com/estudos/_004_/operators/OperadoresAtribuicao.java
+++ src/main/java/br/com/estudos/_004_/operators/OperadoresAtribuicao.java
@@ -2,17 +2,29 @@ package br.com.estudos._004_.operators;
 public class OperadoresAtribuicao {
 
 	/**
-	 *  =  verifica igualdade
+	 *  =  
 	 *  -=
 	 *  +=
 	 *  *=
 	 *  /=
 	 *  %=
-	 *  
+	 *  ++
+	 *  --
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		int contador = 0;
+		
+		//Existe diferença de colocar atribuição (++ e --) atras e na frente da variavel
+		//o valor vai imprimir 0 pois a soma só vai ser feita depois que passar pela execução
+		System.out.println(contador++);
+		
+		//o valor vai imprimir 2 pois a soma só vai ser feita antes que passar pela execução
+		System.out.println(++contador);
 
 	}
 
---
+++ src/main/java/br/com/estudos/_004_1/condicionais/EstruturasCondicionais.java
@@ -0,0 +1,40 @@
+package br.com.estudos._004_1.condicionais;
+
+public class EstruturasCondicionais {
+	public static void main(String[] args) {
+		
+		// IF (SE)
+		//Estrutura: if(condição booleana) {}
+		if(true) {
+			System.out.println("tegdgcgcd");
+		}
+		
+		//saber que pode só para cerificação
+		boolean c = false;
+		if(c = true) {
+			System.out.println("Coisas de certificação");
+		}
+		
+		// IF ELSE
+				//Estrutura: if(condição booleana) {} else  {} 
+				if(false) {
+					System.out.println("tegdgcgcd");
+				} else  {
+					System.out.println("ffdfdfdfd");
+				}
+		
+		
+		// ELSE IF
+		//Estrutura: if(condição booleana) {} else if () {} 
+		if(false) {
+			System.out.println("tegdgcgcd");
+		} else if (true) {
+			System.out.println("ffdfdfdfd");
+		}
+		
+		
+		
+		
+	}
+
+}
