461c1a69afd6ceaaf75e0d12157cde00299b4d6c
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-29 21:26:19 -0300

    20210729

--- src/main/java/br/com/estudos/_007_/oo/Professor.java
+++
@@ -1,7 +0,0 @@
-package br.com.estudos._007_.oo;
-
-public class Professor {
-	public String nome;
-	public int idade;
-	public char sexo;
-}
---
+++ src/main/java/br/com/estudos/_007_/oo/blocosinicializacao/StaticAnime.java
@@ -0,0 +1,51 @@
+package br.com.estudos._007_.oo.blocosinicializacao;
+
+public class StaticAnime {
+    private String nome;
+    private static int[] episodios;
+
+    // 0 - Bloco de inicialização é executado quando a JVM carregar classe
+    // 1 - Alocado espaco em memória pro objeto
+    // 2 - Cada atributo de classe é criado e inicializado com valores default ou o quer for passado
+    // 3 - Bloco de inicialização é executado
+    // 4 - Construtor é executado
+    static {
+        System.out.println("Dentro do bloco de inicializacao estático 1");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao estático 2");
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao estático 3");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicializacao não estático");
+    }
+
+    public StaticAnime(String nome) {
+        this.nome = nome;
+    }
+
+    public StaticAnime() {
+        for (int episodio : StaticAnime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+}
