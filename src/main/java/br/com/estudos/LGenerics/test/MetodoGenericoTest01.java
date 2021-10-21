d4cf3785bc80c8cf26d06570dd2a0cda4a009fbc
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-21 06:26:50 -0300

    Create MetodoGenericoTest01.java

---
+++ src/main/java/br/com/estudos/LGenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,20 @@
+package br.com.estudos.LGenerics.test;
+
+import java.util.List;
+
+import br.com.estudos.LGenerics.dominio.Barco;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Barco> barcoList = criarArrayComUmObjeto(new Barco("Canoa Marota"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrayComUmObjeto(T t) {
+        return List.of(t);
+    }
+//    private static <T extends Comparable<T>> List<T> criarArrayComUmObjeto(T t) {
+//        return List.of(t);
+//    }
+
+}
