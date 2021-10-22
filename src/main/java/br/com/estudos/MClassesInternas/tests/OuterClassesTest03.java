12d37c706c6b2a7e5c003638afb96879c10e7563
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-22 06:44:26 -0300

    ClassesInternas

---
+++ src/main/java/br/com/estudos/MClassesInternas/tests/OuterClassesTest03.java
@@ -0,0 +1,18 @@
+package br.com.estudos.MClassesInternas.tests;
+
+public class OuterClassesTest03 {
+    private String name = "William";
+
+    static class Nested {
+        private String lastName = "Suane";
+
+        void print() {
+            System.out.println(new OuterClassesTest03().name + " " + lastName);
+        }
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest03.Nested nested = new OuterClassesTest03.Nested();
+        nested.print();
+    }
+}
