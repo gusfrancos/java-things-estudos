991751d9c48435d201713d55352c61928e4e8e43
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-10-28 06:03:10 -0300

    Consumer

---
+++ src/main/java/br/com/estudos/NLambdas/dominio/Product.java
@@ -0,0 +1,40 @@
+package br.com.estudos.NLambdas.dominio;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+
+        var sb = new StringBuilder("Product{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
+}
