c3ecb9edcd5ff1be8ac5a84dbe5c755ba98f4548
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-08 07:20:03 -0300

    Collectors

--- src/main/java/br/com/estudos/QStreams/dominio/LightNovel.java
+++ src/main/java/br/com/estudos/QStreams/dominio/LightNovel.java
@@ -2,13 +2,21 @@ package br.com.estudos.QStreams.dominio;
 
 import java.util.Objects;
 
+
 public class LightNovel {
     private String title;
     private double price;
+    private Category category;
 
     public LightNovel(String title, double price) {
         this.title = title;
         this.price = price;
+    }
+
+    public LightNovel(String title, double price, Category category) {
+        this.title = title;
+        this.price = price;
+        this.category = category;
     }
 
     @Override
@@ -29,7 +37,12 @@ public class LightNovel {
         return "LightNovel{" +
                 "title='" + title + '\'' +
                 ", price=" + price +
+                ", category=" + category +
                 '}';
+    }
+
+    public Category getCategory() {
+        return category;
     }
 
     public String getTitle() {
