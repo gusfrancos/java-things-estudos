490c231ed8275ffc49a4c2b7c66b381dc44bd00a
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-22 06:45:51 -0300

    Concorrencia

---
+++ src/main/java/br/com/estudos/SConcorrencia/test/CompletableFutureTest03.java
@@ -0,0 +1,39 @@
+package br.com.estudos.SConcorrencia.test;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import br.com.estudos.SConcorrencia.service.StoreServiceDeprecated;
+
+public class CompletableFutureTest03 {
+    public static void main(String[] args) {
+        StoreServiceDeprecated storeServiceDeprecated = new StoreServiceDeprecated();
+        searchPricesAsyncCompletableFuture(storeServiceDeprecated);
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreServiceDeprecated storeService) {
+        long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(10, r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeService.getPriceSync(s),executor))
+                .collect(Collectors.toList());
+
+        List<Double> prices = completableFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
+        System.out.println(prices);
+        long end = System.currentTimeMillis();
+        executor.shutdown();
+        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
+    }
+}
