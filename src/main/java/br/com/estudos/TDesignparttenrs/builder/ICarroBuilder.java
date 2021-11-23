2021c90ccaa2252ac517ab27d68d1511a03152ef
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-23 12:50:35 -0300

    Builder

---
+++ src/main/java/br/com/estudos/TDesignparttenrs/builder/ICarroBuilder.java
@@ -0,0 +1,13 @@
+package br.com.estudos.TDesignparttenrs.builder;
+
+public interface ICarroBuilder {
+	public void ligar();
+	public void desligar();
+	public void acelerarAte(Integer velocidade);
+	public void mudarMarcha(char marcha);
+	public void parar();
+	public void numeroBancos(Integer numero);
+	public void temGPS(boolean tem);
+	public void temArCondicionado(boolean tem);
+	public void temVidroEletrico(boolean tem);
+}
--- src/main/java/br/com/estudos/TDesignparttenrs/dominio/Carro.java
+++ src/main/java/br/com/estudos/TDesignparttenrs/dominio/Carro.java
@@ -1,30 +1,69 @@
 package br.com.estudos.TDesignparttenrs.dominio;
 
+
 public class Carro {
-	 	private String marca;
-	 	private String cor;
-	 	private String placa;
-	 	private Integer velocidadeAtual;
-	 	private char marchaAtual;
-	 	private Boolean freioDeMaoPuxado;
-	 	private Boolean chaveVirada;
-	 	
-		public Carro(String marca, String cor, String placa, Integer velocidadeAtual, char marchaAtual,
-				Boolean freioDeMaoPuxado, Boolean chaveVirada) {
-			super();
-			this.marca = marca;
-			this.cor = cor;
-			this.placa = placa;
-			this.velocidadeAtual = velocidadeAtual;
-			this.marchaAtual = marchaAtual;
-			this.freioDeMaoPuxado = freioDeMaoPuxado;
-			this.chaveVirada = chaveVirada;
+	private String marca;
+	private String cor;
+	private String placa;
+	private Integer velocidadeAtual;
+	private char marchaAtual;
+	private Boolean freioDeMaoPuxado;
+	private Boolean chaveVirada;
+
+	public Carro(String marca, String cor, String placa, Integer velocidadeAtual, char marchaAtual,
+			Boolean freioDeMaoPuxado, Boolean chaveVirada) {
+
+		this.marca = marca;
+		this.cor = cor;
+		this.placa = placa;
+		this.velocidadeAtual = velocidadeAtual;
+		this.marchaAtual = marchaAtual;
+		this.freioDeMaoPuxado = freioDeMaoPuxado;
+		this.chaveVirada = chaveVirada;
+	}
+
+	@Override
+	public String toString() {
+		return "Carro [marca=" + marca + ", cor=" + cor + ", placa=" + placa + ", velocidadeAtual=" + velocidadeAtual
+				+ ", marchaAtual=" + marchaAtual + ", freioDeMaoPuxado=" + freioDeMaoPuxado + ", chaveVirada="
+				+ chaveVirada + "]";
+	}
+
+	public static final class CarroBuilder {
+		private String marca;
+		private String cor;
+		private String placa;
+		private Integer velocidadeAtual;
+		private char marchaAtual;
+		private Boolean freioDeMaoPuxado;
+		private Boolean chaveVirada;
+
+		public CarroBuilder() {
 		}
 
-		@Override
-		public String toString() {
-			return "Carro [marca=" + marca + ", cor=" + cor + ", placa=" + placa + ", velocidadeAtual="
-					+ velocidadeAtual + ", marchaAtual=" + marchaAtual + ", freioDeMaoPuxado=" + freioDeMaoPuxado
-					+ ", chaveVirada=" + chaveVirada + "]";
+		public CarroBuilder marca(String marca) {
+			this.marca = marca;
+			return this;
 		}
+
+		public CarroBuilder placa(String placa) {
+			this.placa = placa;
+			return this;
+		}
+
+		public CarroBuilder velocidadeAtual(Integer velocidadeAtual) {
+			this.velocidadeAtual = velocidadeAtual;
+			return this;
+		}
+
+		public CarroBuilder marchaAtual(char marchaAtual) {
+			this.marchaAtual = marchaAtual;
+			return this;
+		}
+
+		public Carro build() {
+			return new Carro(marca, cor, placa, velocidadeAtual, marchaAtual, freioDeMaoPuxado, chaveVirada);
+		}
+	}
 }
+		
