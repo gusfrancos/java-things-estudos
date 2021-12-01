d31fcd534a4575952e7ace76bf7e15f8b9da6ca1
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-12-01 07:44:47 -0300

    Docker

--- src/main/java/br/com/estudos/TDesignparttenrs/builder/BuilderPatternTest01.java
+++ src/main/java/br/com/estudos/TDesignparttenrs/builder/BuilderPatternTest01.java
@@ -4,13 +4,13 @@ import br.com.estudos.TDesignparttenrs.dominio.Person;
 
 public class BuilderPatternTest01 {
     public static void main(String[] args) {
-        Person build = new Person.PersonBuilder()
-                .firstName("William")
-                .lastName("Suane")
-                .username("ViradoNoJiraya")
-                .email("william.suane@devdojo.academy")
-                .build();
-        System.out.println(build);
+//        Person build = new Person.PersonBuilder()
+//                .firstName("William")
+//                .lastName("Suane")
+//                .username("ViradoNoJiraya")
+//                .email("william.suane@devdojo.academy")
+//                .build();
+//        System.out.println(build);
 
     }
 }
