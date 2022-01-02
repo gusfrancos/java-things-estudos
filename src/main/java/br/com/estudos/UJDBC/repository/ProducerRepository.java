f257b8ac5feee89705898e5bee3f9ba225fba74f
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2022-01-02 08:40:19 -0300

    jdbc

--- src/main/java/br/com/estudos/UJDBC/repository/ProducerRepository.java
+++ src/main/java/br/com/estudos/UJDBC/repository/ProducerRepository.java
@@ -1,5 +1,7 @@
 package br.com.estudos.UJDBC.repository;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -7,13 +9,14 @@ import java.sql.Statement;
 import br.com.estudos.UJDBC.conn.ConnectionFactory;
 import br.com.estudos.UJDBC.dominio.Producer;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer) {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/br/com/estudos/UJDBC/test/ConnectionFactoryTest01.java
+++ src/main/java/br/com/estudos/UJDBC/test/ConnectionFactoryTest01.java
@@ -5,7 +5,7 @@ import br.com.estudos.UJDBC.repository.ProducerRepository;
 
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("MadHouse").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }
